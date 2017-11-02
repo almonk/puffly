e0374aafacc98d61a68e56247225c2f00285af9f
Alasdair Monk <alasdair.monk@gmail.com>
2017-11-02 15:32:44 +0000

    Convert logic

--- vendor/assets/javascripts/puffly.coffee
+++
@@ -1,4 +0,0 @@
-$(document).ready ->
-  $('#puffly').fadeIn 1000
-  $('#puffly').delay(4000).fadeOut 1000
-  return
---
+++ vendor/assets/javascripts/puffly.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+  $('#puffly').addClass('willAppear')
+  $('#puffly').delay(3000).removeClass('willAppear')
+  return
+});
